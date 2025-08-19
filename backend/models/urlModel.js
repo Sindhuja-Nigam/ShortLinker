bf50c9f99cf8f068110cbe13ca5a1ab8cf719d8f
Sindhuja-Nigam <2k22.csiot.2212420@gmail.com>
2025-08-19 09:54:44 -0700

    Add files via upload

---
+++ backend/models/urlModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  longUrl: { type: String, required: true },
+  shortUrl: { type: String, required: true, unique: true },
+  clicks: { type: Number, default: 0 }
+});
+
+const Url = mongoose.model("Url", urlSchema);
+export default Url;
